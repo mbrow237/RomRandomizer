efff740cfb3d3a242ee2628b0db6338c8386ce02
mbrow237 <mbrow237@uncc.edu>
2016-04-29 23:04:08 -0400

    Working GUI v 1.00
    A very basic working GUI, haven't had much testing done. Click Load first, then hit Random when it's done. The code is very sloppy because this is my first GUI I made without Windowbuilder. I will clean it up when I get the chance.

--- src/mainGUI.java
+++ src/mainGUI.java
@@ -1,6 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.ComponentOrientation;
 import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,26 +14,132 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField; 
 
-public class mainGUI {
+
+public class mainGUI extends JFrame implements WindowListener,ActionListener {
+	JButton bntRandom;
+	JButton bntLoad;
+	JLabel lblStatus;
+	RecursTest rt = new RecursTest();
+	
 	public static void main(String[] args) {  
-		JFrame frame = new JFrame("Rom Randomizer");
-        frame.setSize(350, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainGUI startWindow = new mainGUI("Rom Randomizer");
+		startWindow.setSize(350,200);
+		startWindow.setVisible(true);
+		
+		
+		//JFrame frame = new JFrame("Rom Randomizer");
+        //frame.setSize(350, 200);
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        JPanel panel = new JPanel();
-        FlowLayout panelFlowLayout = new FlowLayout();
+        //JPanel panel = new JPanel();
+        //FlowLayout panelFlowLayout = new FlowLayout();
         //panelFlowLayout.setVgap(50);
-        panel.setLayout(panelFlowLayout);
-        frame.add(panel);
+        //panel.setLayout(panelFlowLayout);
+        //frame.add(panel);
         
         
-        JButton bntRandom = new JButton("Randomize");
-        JButton bntLoad = new JButton("Load Roms");
+        //addWindowListner(this);
+        //JButton bntRandom = new JButton("Randomize");
+        //bntRandom.setEnabled(false);
+        //JButton bntLoad = new JButton("Load Roms");
         
-        panel.add(bntLoad);
-        panel.add(bntRandom);
+        //panel.add(bntLoad);
+        //panel.add(bntRandom);
         //panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         
-        frame.setVisible(true);
+        //frame.setVisible(true);
 	}
+
+	
+	public mainGUI(String title){
+		super(title);
+		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		FlowLayout panelFlowLayout = new FlowLayout();
+		setLayout(panelFlowLayout);
+		bntRandom = new JButton("Randomize");
+		bntRandom.setEnabled(false);
+		bntLoad = new JButton("Load");
+		lblStatus = new JLabel("Click Load");
+		add(bntLoad);
+		add(bntRandom);
+		add(lblStatus);
+		
+		bntLoad.addActionListener(this);
+		bntRandom.addActionListener(new randomAction(rt));
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		try {
+			lblStatus.setText("Loading ROMS");
+			rt.loadRoms();
+			bntRandom.setEnabled(true);
+			lblStatus.setText("Loading Complete, Click Random");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		dispose();
+        System.exit(0);
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+class randomAction implements ActionListener { 
+RecursTest rt;
+public randomAction(RecursTest obj)
+{
+	rt = obj;
+}
+	
+	public void actionPerformed(ActionEvent e) {
+		 try {
+			rt.pickRandomRom();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	    }
+	
 }
